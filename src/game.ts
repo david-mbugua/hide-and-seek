f9e5d3ab27b43324ad3577274d52699f54910ed7
RaphaelNdonga <nkibi53@gmail.com>
2023-01-17 10:21:06 +0300

    rotator system returned

--- src/game.ts
+++ src/game.ts
@@ -4,29 +4,29 @@ import { SpawnCube } from "./SpawnCube"
 import { TriggerButton } from "./triggerButton"
 import { movePlayerTo } from "@decentraland/RestrictedActions"
 
-// class RotatorSystem {
-//   camera = new Camera()
-//   ws = new WebSocket("ws://localhost:8080")
-//   // this group will contain every entity that has a Transform component
-//   group = engine.getComponentGroup(Transform)
-//   time: number = 0
-//   update(dt: number) {
-//     this.time+=dt
-//     if(this.time>1) {this.ws.send(JSON.stringify({ethAddress: "0x123", status: "seeker", position: this.camera.position.asArray()}))
-//   this.time = 0}
-//     // iterate over the entities of the group
-//     for (const entity of this.group.entities) {
-//       // get the Transform component of the entity
-//       const transform = entity.getComponent(Transform)
+class RotatorSystem {
+  camera = new Camera()
+  ws = new WebSocket("ws://localhost:8080")
+  // this group will contain every entity that has a Transform component
+  group = engine.getComponentGroup(Transform)
+  time: number = 0
+  update(dt: number) {
+    this.time+=dt
+    if(this.time>1) {this.ws.send(JSON.stringify({ethAddress: "0x123", status: "seeker", position: this.camera.position.asArray()}))
+  this.time = 0}
+    // iterate over the entities of the group
+    for (const entity of this.group.entities) {
+      // get the Transform component of the entity
+      const transform = entity.getComponent(Transform)
 
-//       // mutate the rotation
-//       transform.rotate(Vector3.Up(), dt * 10)
-//     }
-//   }
-// }
+      // mutate the rotation
+      transform.rotate(Vector3.Up(), dt * 10)
+    }
+  }
+}
 
-// // Add a new instance of the system to the engine
-// engine.addSystem(new RotatorSystem())
+// Add a new instance of the system to the engine
+engine.addSystem(new RotatorSystem())
 
 /// --- Spawn a cube ---
 
